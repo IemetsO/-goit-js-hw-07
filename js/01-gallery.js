d3ffcb037b562cfa0e730c48b3b861f49f26c2aa
IemetsO <oksana.v.iemets@gmail.com>
2022-02-23 16:01:17 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-galleryItems.preview
+galleryItems.preview;
 const gallery = document.querySelector(".gallery");
 
-const itemsInGallery = createGalleryItems(galleryItems)
+const itemsInGallery = createGalleryItems(galleryItems);
 
-gallery.insertAdjacentHTML('beforeend', itemsInGallery);
+gallery.insertAdjacentHTML("beforeend", itemsInGallery);
 
 function createGalleryItems(galleryItems) {
-    return galleryItems.map(({ preview, original, description}) => `<div class="gallery__item">
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
     <a class="gallery__link" href="large-image.jpg">
       <img
         class="gallery__image"
@@ -18,35 +20,39 @@ function createGalleryItems(galleryItems) {
         alt="${description}"
       />
     </a>
-  </div>`). join(" ")
+  </div>`
+    )
+    .join(" ");
 }
 
 gallery.addEventListener("click", clickOnGalleryItem);
 
-function clickOnGalleryItem(event){
-    
-    if (event.target.nodeName !== 'IMG'){
-        return
-    }
-event.preventDefault();
-openModal(event.target.dataset.source);
+function clickOnGalleryItem(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  openModal(event.target.dataset.source);
 }
 
 function openModal(src) {
-    const instance = basicLightbox.create(
-        `
+  const instance = basicLightbox.create(
+    `
             <img src="${src}" width="800" height="600">
-        `,          {
-            onShow: () => {
-              window.addEventListener("keydown", onEscClick);
-            },
-            onClose: () => window.removeEventListener("keydown",  onEscClick),
-          }
-      );
-function onEscClick(event) {
-if (event.code === 'Escape') {
-  instance.close();
-  }  
+        `,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscClick);
+      },
+      onClose: () => window.removeEventListener("keydown", onEscClick),
+    }
+  );
+  function onEscClick(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
 }
-instance.show()
-};
+
+
